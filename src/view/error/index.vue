003b7e125949f47e5155371645dc14c648534fbe
GodX <1046529973@qq.com>
2024-06-06 11:42:32 +0800

    async

---
+++ src/view/error/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <van-empty image="error" description="未找到该页面">
+    <van-button round type="primary" @click="back">带我回家</van-button>
+  </van-empty>
+</template>
+
+<script lang="ts" setup>
+const router = useRouter();
+const back = () => {
+  router.push('/home');
+};
+</script>
